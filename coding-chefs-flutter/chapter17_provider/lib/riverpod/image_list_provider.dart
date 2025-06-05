a673b427db2b2b15ebb676f27308156c41bf6019
gaeunpark7 <hen77360@gmail.com>
2025-06-05 18:30:05 +0900

    image-provider

---
+++ coding-chefs-flutter/chapter17_provider/lib/riverpod/image_list_provider.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ImageListNotifier extends StateNotifier<List<String>> {
+  ImageListNotifier() : super([]);
+
+  void addImage() {
+    final randomImage = Random().nextInt(1000);
+    final ImageUrl = 'https://picsum.photos/seed/$randomImage/200/300';
+    state = [...state, ImageUrl];
+  }
+
+  void removeImage(int index) {
+    if (index >= 0 && index < state.length) {
+      final newList = List<String>.from(state); //기존리스트 복사
+      state = newList; //새 리스트 상태로 업데이트
+    }
+  }
+
+  final imageListNotifier =
+      StateNotifierProvider<ImageListNotifier, List<String>>(
+        (ref) => ImageListNotifier(),
+      );
+}
