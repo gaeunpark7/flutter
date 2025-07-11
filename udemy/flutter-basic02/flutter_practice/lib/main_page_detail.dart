2ff1727a784fa90582dee0ca79ce6de7f6f28b0d
gaeunpark7 <hen77360@gmail.com>
2025-07-11 19:52:13 +0900

    dialog

--- udemy/flutter-basic02/flutter_practice/lib/main_page_detail.dart
+++ udemy/flutter-basic02/flutter_practice/lib/main_page_detail.dart
@@ -8,46 +8,84 @@ class MainPageDetail extends StatefulWidget {
 }
 
 class _MainPageDetailState extends State<MainPageDetail> {
-  List<String> lstMenu = ["농담곰", "고로케", "퍼그상"];
-  String strSelectMenu = "농담곰";
+  String selectedFruit = "사과";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Container(),
+        title: const Text("test"),
         backgroundColor: Colors.blueGrey,
         actions: [
           PopupMenuButton<String>(
-            onSelected: (value) {
-              if (value == 'edit') {
-              } else {}
-            },
-            itemBuilder: (context) => [
-              const PopupMenuItem(value: 'edit', child: Text("수정")),
-              const PopupMenuDivider(),
-              const PopupMenuItem(value: 'delete', child: Text("삭제")),
-            ],
-            // icon: const Icon(Icons.more_vert),
-          )
+              onSelected: (value) {
+                if (value == 'edit') {
+                } else if (value == 'delete') {}
+              },
+              itemBuilder: (ctx) => [
+                    const PopupMenuItem(value: 'edit', child: Text("수정")),
+                    const PopupMenuDivider(),
+                    const PopupMenuItem(value: 'delete', child: Text("삭제"))
+                  ])
         ],
       ),
-      body: Column(
-        children: [
-          DropdownButton(
-              value: strSelectMenu,
-              items: lstMenu
-                  .map((e) => DropdownMenuItem(
-                        value: e,
-                        child: Text(e),
-                      ))
+      body: Center(
+        child: Column(children: [
+          Text("선택한 과일: $selectedFruit"),
+          DropdownButton<String>(
+              value: selectedFruit,
+              items: ['사과', '포도', '바나나']
+                  .map((e) => DropdownMenuItem(value: e, child: Text(e)))
                   .toList(),
-              onChanged: (value) {
-                strSelectMenu = value ?? lstMenu[0];
-                print("105:$value");
-                setState(() {});
-              })
-        ],
+              onChanged: (String? newValue) {
+                if (newValue != null) {
+                  setState(() {
+                    selectedFruit = newValue;
+                  });
+                }
+              }),
+          //alertDialog
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: const Text("알람"),
+                          content: const Text("안녕하세요"),
+                          actions: [
+                            TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text("확인")),
+                            TextButton(
+                                onPressed: () {}, child: const Text("저장"))
+                          ],
+                        ));
+              },
+              child: const Text("AlertDialog")),
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (ctx) => Dialog(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Text("Dialog"),
+                                const SizedBox(height: 20),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("닫기"))
+                              ],
+                            ),
+                          ),
+                        ));
+              },
+              child: const Text("Dialog"))
+        ]),
       ),
     );
   }
